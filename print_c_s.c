aba24bdf612c5dbf44c5be970c4b896afcbf6a7a
Antonio Estela <antonioestela73@gmail.com>
2020-03-18 01:33:39 +0000

    Added prin_c_s.c file

---
+++ print_c_s.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * print_c - function that print a single character.
+ * @list: arguments to print
+ * Return: number of elements printed
+ */
+int print_c(va_list list)
+{
+	char s = va_arg(list, int);
+	int i = 0;
+
+	/*  i = write(1, &s, 1); */
+	_putchar(s + i);
+	return (i);
+}
+/**
+ * print_s - function that print a string.
+ * @list: arguments to print
+ * Return: number of elements printed
+ */
+int print_s(va_list list)
+{
+	char *str = va_arg(list, char *);
+	int i = 0;
+
+	if (str == NULL)
+	{
+		str = "NULL";
+	}
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	return (i);
+}
